aa50e87d6a45188dfd5bcb9800657774b885b4c8
GDZ <735900200@qq.com>
2019-12-31 09:43:30 +0800

    info

--- src/view/index/index.vue
+++ src/view/index/index.vue
@@ -1,13 +1,103 @@
 <template>
-  <div>index</div>
+  <div class="layout">
+    <Sider ref="side1" :style="{position: 'fixed', height: '100vh', left: 0, overflow: 'auto'}">
+      <div class="index-MenuName">仓储管理系统</div>
+      <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
+        <Submenu name="1">
+          <template slot="title">
+            <Icon type="ios-navigate"></Icon>
+            Item 1
+          </template>
+          <MenuItem name="1-1">Option 1</MenuItem>
+          <MenuItem name="1-2">Option 2</MenuItem>
+          <MenuItem name="1-3">Option 3</MenuItem>
+        </Submenu>
+        <Submenu name="2">
+          <template slot="title">
+            <Icon type="ios-keypad"></Icon>
+            Item 2
+          </template>
+          <MenuItem name="2-1">Option 1</MenuItem>
+          <MenuItem name="2-2">Option 2</MenuItem>
+        </Submenu>
+        <Submenu name="3">
+          <template slot="title">
+            <Icon type="ios-analytics"></Icon>
+            Item 3
+          </template>
+          <MenuItem name="3-1">Option 1</MenuItem>
+          <MenuItem name="3-2">Option 2</MenuItem>
+        </Submenu>
+      </Menu>
+    </Sider>
+    <Layout :style="{marginLeft: '200px'}">
+      <Header class="Header-div">
+        <div class="Header-view row-between">
+          <div></div>
+          <div class="row-div">
+            <div class="user-name hover-div">
+              <span>工号: 004500</span>
+            </div>
+            <Icon type="md-power hover-div" size="20"/>
+          </div>
+        </div>
+      </Header>
+      <Content :style="{padding: '0 16px 16px'}">
+        <Breadcrumb :style="{margin: '16px 0'}">
+          <BreadcrumbItem>Home</BreadcrumbItem>
+          <BreadcrumbItem>Components</BreadcrumbItem>
+          <BreadcrumbItem>Layout</BreadcrumbItem>
+        </Breadcrumb>
+        <Card>
+          <div style="height: 1500px">Content</div>
+        </Card>
+      </Content>
+    </Layout>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'index'
+  name: 'index',
+  data () {
+    return {
+      isCollapsed: false
+    }
+  },
+  computed: {
+    rotateIcon () {
+      return [
+        'menu-icon',
+        this.isCollapsed ? 'rotate-icon' : ''
+      ]
+    }
+  },
+  methods: {
+    collapsedSider () {
+      this.$refs.side1.toggleCollapse()
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .index-MenuName{
+    width: 100%;
+    color: white;
+    padding: 15px 0;
+    text-align: center;
+  }
+  .Header-div {
+    background: white;
+    boxShadow: 0 2px 3px 2px rgba(0,0,0,.1);
+    padding: 0;
+  }
+  .Header-view{
+    width: 100%;
+    height: 100%;
+    padding: 0 20px;
+  }
+  .user-name{
+    margin-right: 20px;
+  }
 </style>
--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -1,15 +1,108 @@
 <template>
-  <div>
-    loginsd
+  <div class="login login-row">
+    <div class="login-Form">
+      <p style="margin-bottom: 33px;font-size: 14px;">仓储管理系统</p>
+      <Form ref="formItem" :model="formItem" :rules="ruleItem">
+        <FormItem label="" prop="code">
+          <Input v-model="formItem.code" placeholder="账号">
+            <Icon type="ios-contact" slot="prepend" size="20"/>
+          </Input>
+        </FormItem>
+        <FormItem label="" prop="password">
+          <Input v-model="formItem.password" type="password" placeholder="密码">
+            <Icon type="md-lock" slot="prepend" size="20"/>
+          </Input>
+        </FormItem>
+        <FormItem >
+          <Button type="warning" long style="height: 40px" class="login-Button" @click="on_login" :loading="buttomLogding">登录</Button>
+        </FormItem>
+      </Form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'login'
+  name: 'login',
+  data () {
+    return {
+      buttomLogding: false,
+      formItem: {
+        code: '',
+        password: ''
+      },
+      ruleItem: {
+        code: [
+          { required: true, message: '用户名不能为空', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '密码不能为空', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    on_login () {
+      this.$refs['formItem'].validate((valid) => {
+        if (valid) {
+          this.loginRequest()
+        } else {
+          this.$Modal.warning({
+            title: '错误提示',
+            content: '请输入用户名或密码'
+          })
+        }
+      })
+    },
+    loginRequest () {
+      this.buttomLogding = true
+      this.$ajax.post('/user/login', this.formItem).then(res => {
+        console.log(res)
+        if (res.data.code === 200) {
+          this.$cookies.set('token', res.data.data)
+        }
+        this.buttomLogding = false
+      }).catch((e) => {
+        this.buttomLogding = false
+        console.log(e, '/user/login')
+        this.$Notice.warning({title: '警告', desc: '接口错误,请与系统管理员联系,/user/login'})
+      })
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped >
+  .login-row{
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+  }
+  .login{
+    width: 100vw;
+    height: 100vh;
+    background-image: url("../../assets/3@2x.png");
+    background-size: 100% 100%;
+  }
+  .login-Form{
+    width: 300px;
+    margin-right: 20%;
+  }
+  .login-register-p{
+    color:  #888888;
+  }
+  .login-register-p>span{
+    color:  #333333;
+  }
+  .login-register-p:hover>span{
+    color: red;
+  }
+  .login--p-hover:hover{
+    cursor: pointer;
+  }
+  .login-Button{
+    letter-spacing: 5px;
+    font-size: 16px;
+  }
 </style>
